ef1e5c2f9972de6676813abca08c053183e63ea0
Akars <williamli973@gmail.com>
2022-12-07 13:30:19 +0100

    progress ex5

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { node: true },
+  parser: '@typescript-eslint/parser',
+  plugins: [ '@typescript-eslint' ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb-base'
+  ],
+  rules: {
+    "import/extensions": 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-undef': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off'
+  }
+}
