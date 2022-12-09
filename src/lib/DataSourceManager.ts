aba88d44014b1d8b7e5a21e2fe928219701864f9
Akars <williamli973@gmail.com>
2022-12-09 20:45:31 +0100

    exercise 6 - using dependency injection

---
+++ src/lib/DataSourceManager.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line max-classes-per-file
+import { Service } from 'typedi';
+import { AppDataSource } from '../DataSource';
+
+@Service()
+class DataSourceInjected {
+  getRepository() {
+    return AppDataSource.manager;
+  }
+}
+
+@Service()
+export class Manager {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    // eslint-disable-next-line no-unused-vars
+    public injectedResources: DataSourceInjected,
+    // eslint-disable-next-line no-empty-function
+  ) {}
+}
