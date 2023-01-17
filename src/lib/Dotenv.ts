cb83ccdf4494b561a3aa14a4a6c3b62660594080
Akars <williamli973@gmail.com>
2023-01-17 11:17:06 +0100

    update eslint sync true

--- src/lib/Dotenv.ts
+++ src/lib/Dotenv.ts
@@ -15,7 +15,7 @@ export const DATABASE_HOST = getOrThrow('DATABASE_HOST');
 export const DATABASE_NAME = getOrThrow('DATABASE_NAME');
 export const DATABASE_PWD = getOrThrow('DATABASE_PWD');
 export const DATABASE_PORT = parseInt(getOrThrow('DATABASE_PORT'), 10);
-export const DS_SYNC = false;
+export const DS_SYNC = true;
 
 export const FASTIFY_PORT = parseInt(getOrThrow('FASTIFY_PORT'), 10);
 export const FASTIFY_ADDR = getOrThrow('FASTIFY_ADDR');
